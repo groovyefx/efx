028e81af5b1d56c7e1893ce47308590cfa45b8b9
lovefree <ty80style@126.com>
2013-06-18 10:40:35 +0800

    added LogUtil.java for capturing exception log

---
+++ efx/src/efx/util/LogUtil.java
@@ -0,0 +1,52 @@
+package efx.util;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author Franklyn Donald <lovefree103@gmail.com>
+ *
+ */
+public class LogUtil {
+
+    private final static Logger logger = LoggerFactory.getLogger(LogUtil.class);
+
+    public static void initErrorLogStream() {
+        PrintStream syserr = System.err;
+        try {
+            Log4jErrorPrintStream errStream = new Log4jErrorPrintStream(System.err);
+            System.setErr(errStream);
+        } catch (Exception e) {
+            System.setErr(syserr);
+        }
+    }
+
+    public static String toString(Throwable e) {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter, true);
+        e.printStackTrace(printWriter);
+        printWriter.close();
+        return stringWriter.toString();
+    }
+
+    private static class Log4jErrorPrintStream extends PrintStream {
+
+        Log4jErrorPrintStream(OutputStream out) {
+            super(out, true); 
+        }
+
+        public void println(Object obj) {
+            if (obj instanceof Throwable) {              
+                if (logger != null) {
+                	logger.error(LogUtil.toString((Throwable) obj));
+                }
+            }
+        }
+    }
+}
